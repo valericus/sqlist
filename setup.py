3b96d5953ee47d8a86c98b5d373940c5b68db79c
Valery Ryaboshapko <vr@exante.eu>
2016-01-11 21:41:59 +0300

    prepared for publishing on PyPI

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlist',
+    version='0.1a',
+    packages = find_packages(),
+    description='List-like wrapper for SQLite',
+    long_description=open('README.rst').read(),
+
+    author='Valery Ryaboshapko',
+    author_email='valera@creator.su',
+    license='Python Software Foundation License',
+    url='https://github.com/valericus/sqlist',
+
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+    ],
+
+    zip_safe=True
+)
